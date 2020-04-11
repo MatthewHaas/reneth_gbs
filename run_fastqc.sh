204ce86e500e08702eca517f8bffe7476cc39bb3
Matthew Haas <matthew.haas1@gmail.com>
2020-04-11 09:38:16 -0500

    Initial commit
    Commit of file to run fastQC on Reneth's GBS data

---
+++ run_fastqc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#PBS -l nodes=1:ppn=8,mem=30g,walltime=24:00:00
+#PBS -m abe
+#PBS -M haasx092@umn.edu
+#PBS -o run_fastqc.out
+#PBS -e run_fastqc.err
+#PBS -N run_fastqc
+
+cd /home/jkimball/haasx092/reneth_gbs
+
+module load fastqc
+
+# Run fastQC on trimmed reads
+for i in $(cat 200410_sample_list.txt); do
+fastqc $i/${i}_trimmed.fastq.gz;
+done
