b2cf6be02a23891c6d8b7f90b7941981acb6eb37
Matthew Haas <matthew.haas1@gmail.com>
2020-04-10 19:26:42 -0500

    Fixed exit status = -9
    Exit status -9 was actually caused by the inability of script to create the standard error/output files. That's fixed, but now am getting an error status of 2.

--- run_cutadapt.sh
+++ run_cutadapt.sh
@@ -2,8 +2,8 @@
 #PBS -l nodes=1:ppn=8,mem=30g,walltime=24:00:00
 #PBS -m abe
 #PBS -M haasx092@umn.edu
-#PBS -o ~/reneth_gbs/run_cutadapt.out
-#PBS -e ~/reneth_gbs/run_cutadapt.err
+#PBS -o run_cutadapt.out
+#PBS -e run_cutadapt.err
 cd ~/reneth_gbs
 module load cutadapt
 for i in $(cat 200410_sample_list.txt); do
